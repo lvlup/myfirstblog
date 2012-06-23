0dc02158eaaafb22236bca966167fd9eb0949dea
Rus <ad@LENOVO.(none)>
2012-06-23 12:26:38 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog2::Application.config.secret_token = '8586e77e1b25a1a36df3d06cbf950194745fcca1f20d0147203b1c39a0a9c77210aaff66ef93d0fee316ca41ce06bc7aaf6800f56c4931166b15ca707199f322'
---
+++ db/migrate/20120621122005_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :string
+      t.string :title
+      t.string :content
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
